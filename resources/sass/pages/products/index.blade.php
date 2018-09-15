7ed05bad5323aa79a693cac9c757d6e59bc35f5a
Keith Gulbro <keith.gulbro@irishtitan.com>
2018-09-15 14:28:51 -0500

    Update basic page contents

---
+++ resources/sass/pages/products/index.blade.php
@@ -0,0 +1,14 @@
+TODO:
+Add Sidebar
+Add Products Foreach
+Add Product Actions
+Add Pagination
+
+<div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="..." alt="Card image cap">
+    <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+</div>
